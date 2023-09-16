db3e5904f70db2bf81bfa0fa2ae844b20a11b5d8
cyborg999 <sadiwajordan1991@gmail.com>
2023-09-17 03:13:44 +0800

    Added layout to login, register

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,6 +1,13 @@
-<html>
-    <body>
-    <h1>Login</h1>
+@extends('layouts.default')
+@section('title', 'SadiwaPOS - Login')
+@section('content')
+  <div class="container col-xl-10 col-xxl-8 px-4 py-5">
+    <div class="row align-items-center g-lg-5 py-5">
+      <div class="col-lg-7 text-center text-lg-start">
+        <h1 class="display-4 fw-bold lh-1 mb-3">Login</h1>
+        <p class="col-lg-10 fs-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio eaque dolorem recusandae quidem, eveniet possimus unde alias quaerat, explicabo amet earum harum, molestias consequatur repudiandae pariatur provident quos assumenda. Sint!</p>
+      </div>
+      <div class="col-md-10 mx-auto col-lg-5">
         @if ($errors->any())
             <div class="alert alert-danger">
                 <ul>
@@ -10,15 +17,23 @@
                 </ul>
             </div>
         @endif
-        <form method="post" action="/login">
+        <form class="p-4 p-md-5 border rounded-3 bg-light" method="post" action="/login">
             @csrf
-            <label>Email 
-                <input type="text" name="email"/>
-            </label>
-            <label>Password 
-                <input type="password" name="password"/>
-            </label>
-            <input type="submit" value="Login"/>
+          <div class="form-floating mb-3">
+            <input type="email" class="form-control" id="floatingInput" name="email" placeholder="name@example.com">
+            <label for="floatingInput">Email address</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input type="password" class="form-control" id="floatingPassword" name="password" placeholder="Password">
+            <label for="floatingPassword">Password</label>
+          </div>
+          <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
+          <hr class="my-4">
+          <small class="text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis similique minus doloremque. Cum doloribus praesentium sit quaerat earum. Ut perspiciatis neque accusantium nisi dolorem sed odit voluptatem reiciendis delectus similique.</small>
         </form>
-    </body>
+      </div>
+    </div>
+  </div>
+@stop
+  </body>
 </html>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,6 +1,13 @@
-<html>
-    <body>
-        <h1>Register</h1>
+@extends('layouts.default')
+@section('title', 'SadiwaPOS - SignUp')
+@section('content')
+  <div class="container col-xl-10 col-xxl-8 px-4 py-5">
+    <div class="row align-items-center g-lg-5 py-5">
+      <div class="col-lg-7 text-center text-lg-start">
+        <h1 class="display-4 fw-bold lh-1 mb-3">Register an account</h1>
+        <p class="col-lg-10 fs-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio eaque dolorem recusandae quidem, eveniet possimus unde alias quaerat, explicabo amet earum harum, molestias consequatur repudiandae pariatur provident quos assumenda. Sint!</p>
+      </div>
+      <div class="col-md-10 mx-auto col-lg-5">
         @if ($errors->any())
             <div class="alert alert-danger">
                 <ul>
@@ -10,13 +17,27 @@
                 </ul>
             </div>
         @endif
-
-        <form method="post" action="/register">
+        <form class="p-4 p-md-5 border rounded-3 bg-light" method="post" action="/register">
             @csrf
-            <input type="text" name="name" placeholder="Name" >
-            <input type="email" name="email" placeholder="Email" >
-            <input type="password" name="password" placeholder="Password" >
-            <button type="submit">Register</button>
+          <div class="form-floating mb-3">
+            <input type="text" class="form-control" id="floatingName" name="name" placeholder="Name">
+            <label for="floatingName">Name</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input type="email" class="form-control" id="floatingInput" name="email" placeholder="name@example.com">
+            <label for="floatingInput">Email address</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input type="password" class="form-control" id="floatingPassword" name="password" placeholder="Password">
+            <label for="floatingPassword">Password</label>
+          </div>
+          <button class="w-100 btn btn-lg btn-primary" type="submit">Sign Up</button>
+          <hr class="my-4">
+          <small class="text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis similique minus doloremque. Cum doloribus praesentium sit quaerat earum. Ut perspiciatis neque accusantium nisi dolorem sed odit voluptatem reiciendis delectus similique.</small>
         </form>
-    </body>
+      </div>
+    </div>
+  </div>
+@stop
+  </body>
 </html>
